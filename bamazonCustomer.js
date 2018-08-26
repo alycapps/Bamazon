2bbe72bb39b6c884a78183fbf4e49b9815a37044
Aly <38168385+alycapps@users.noreply.github.com>
2018-08-26 13:37:37 -0400

    added choosing id and quantity to customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,20 +17,21 @@ var customer = function() {
         if(err) throw err;
         readlistings();
     })
-    console.log("yay, you connected to the customer function!");
 };
 
 //function to read current listings
 var readlistings = function() {
-    console.log("function to read listings ran");
     conn.query("select * from products", function(err, res) {
         if (err) throw err;
-        console.log(res);
+        for (var m=0; m<res.length; m++) {
+            console.log(`ID: ${res[m].item_id}, Product: ${res[m].product_name}, Price: $${res[m].price}`);
+        }
         custInquirerCall();
     })
 };
 
 //prompt users to enter info for items and quantity they want to buy
+var chosenId, chosenUnits;
 var custInquirerCall = function() {
     inquirer.prompt([
         {
@@ -45,13 +46,38 @@ var custInquirerCall = function() {
         }
     ]).then(function(response) {
         console.log("woohoo made it through the questions");
+        chosenId = response.id;
+        console.log("chosen id: " + chosenId);
+        chosenUnits = response.units;
+        console.log("chosen units: " + chosenUnits);
+        custUpdate();
+    })
+};
 
-        conn.end();
+var custUpdate = function() {
+    console.log("Now we are in the updating price function :)")
+        //search through database for quantity of id entered
+        conn.query("select stock_quantity from products where ?", 
+        {
+            item_id: chosenId
+        }, function(err, data) {
+            console.log("quantity of chosen" + JSON.stringify(data[0].stock_quantity));
+            if (err) throw err;
+            if(chosenUnits > data[0].stock_quantity) {
+                console.log("you requested more than we have");
+            }
+            else {
+                console.log("ok buy it");
+            }
+        })
+        //checks quantity of id chosen 
+        //if less than it buys
+        //if more isufficient funds
 
         //start over and see what view they would like to use
         // index();
-    })
 };
+
 
 
 module.exports = customer;
