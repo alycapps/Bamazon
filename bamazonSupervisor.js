3f7fad20fdeb5054b7f282f6a73e14005f5daf2d
Aly <38168385+alycapps@users.noreply.github.com>
2018-08-27 21:31:08 -0400

    supervisor under construction notification

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,5 @@
 var supervisor = function() {
-    console.log("yay, you connected to the supervisor function!");
+    console.log("This functionality is still under construction, please check back later");
 }
 
 module.exports = supervisor;
